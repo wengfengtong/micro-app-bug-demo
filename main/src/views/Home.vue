8f5e56c12ec846405f05b795e31f60b9785bc24a
wengfengtong <17787718318@qq.com>
2022-01-11 22:05:08 +0800

    first commit

---
+++ main/src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="dialog">
+      <micro-app
+        name="app1"
+        url="http://192.168.108.4:9528/#/dashboard"
+      ></micro-app>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style  scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background: black;
+}
+.dialog {
+  overflow: hidden;
+  width: 1200px;
+  height: 820px;
+  position: absolute;
+  top: 50px;
+  left: 200px;
+  background: white;
+  border-radius: 20px;
+  transform: translate(0, 0);
+}
+</style>
